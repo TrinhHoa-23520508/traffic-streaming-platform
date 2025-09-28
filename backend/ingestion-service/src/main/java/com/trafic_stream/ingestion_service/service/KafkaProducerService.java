825cff56b769bdbc411b1a86011d4091534c2fc0
Yuhel187 <23520610@gm.uit.edu.vn>
2025-09-28 22:04:08 +0700

    feat:add example ingestion-service

---
+++ backend/ingestion-service/src/main/java/com/trafic_stream/ingestion_service/service/KafkaProducerService.java
@@ -0,0 +1,41 @@
+package com.trafic_stream.ingestion_service.service;
+
+import com.trafic_stream.ingestion_service.config.KafkaTopicConfig;
+import com.trafic_stream.ingestion_service.dto.CameraDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaProducerService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaProducerService.class);
+
+    private final KafkaTemplate<String, CameraDTO> kafkaTemplate;
+
+    public KafkaProducerService(KafkaTemplate<String, CameraDTO> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendTrafficData(CameraDTO data) {
+        String key = data.getCameraId();
+        data.setTimestamp(System.currentTimeMillis());
+        LOGGER.info("Đang gửi dữ liệu camera {} vào Kafka topic {} ",
+                key, KafkaTopicConfig.TRAFFIC_TOPIC);
+
+        kafkaTemplate.send(KafkaTopicConfig.TRAFFIC_TOPIC, key, data)
+                .whenComplete((result, ex) -> {
+                    if (ex == null) {
+                        LOGGER.info("Gửi thành công: topic={}, partition={}, offset={}",
+                                result.getRecordMetadata().topic(),
+                                result.getRecordMetadata().partition(),
+                                result.getRecordMetadata().offset());
+                    } else {
+                        LOGGER.error("Lỗi khi gửi dữ liệu camera {} vào Kafka", key, ex);
+                    }
+                });
+    }
+
+
+}

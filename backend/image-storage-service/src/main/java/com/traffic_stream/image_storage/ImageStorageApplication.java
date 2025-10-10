6c030f688590e4e022af15a46bc03f7b6b683b47
Duong Hieu <hieuduong181005@gmail.com>
2025-10-10 23:53:22 +0700

    feat(image-storage): make image streaming async

--- backend/image-storage-service/src/main/java/com/traffic_stream/image_storage/ImageStorageApplication.java
+++ backend/image-storage-service/src/main/java/com/traffic_stream/image_storage/ImageStorageApplication.java
@@ -3,15 +3,17 @@ package com.traffic_stream.image_storage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableAsync
 public class ImageStorageApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ImageStorageApplication.class, args);
     }
-    
+
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();

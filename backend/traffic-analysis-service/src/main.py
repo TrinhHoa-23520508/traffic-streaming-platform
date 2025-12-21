8e92fe11768b04b24ba1e6c4c7356feb866acf4f
Yuhel187 <23520610@gm.uit.edu.vn>
2025-12-21 15:54:59 +0700

    feat: simple monitoring

--- backend/traffic-analysis-service/src/main.py
+++ backend/traffic-analysis-service/src/main.py
@@ -4,7 +4,12 @@ from config import KAFKA_BROKER, KAFKA_INPUT_TOPIC, KAFKA_OUTPUT_TOPIC
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
+from prometheus_client import start_http_server
+
 def main():
+    # Start Prometheus metrics server on port 8000
+    start_http_server(8000)
+    logging.info("Prometheus metrics server started on port 8000")
     service = TrafficAnalysisService(
         kafka_broker=KAFKA_BROKER,
         input_topic=KAFKA_INPUT_TOPIC,
